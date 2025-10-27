9a9f531a65e088ab29bf3f0637af41a98689a7b0
wiwink-hub <ritamaya857@gmail.com>
2025-10-27 21:04:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+   base: "/",
   server: {
     host: "0.0.0.0",
     port: 5000,
@@ -15,9 +15,5 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
   },
 }));
